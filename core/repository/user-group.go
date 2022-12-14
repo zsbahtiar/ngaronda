9ffd090ea868954abcae39a3e106d9a65f8e03de
Zam Zam Saeful Bahtiar <zamzam.saeful@efishery.com>
2022-12-15 05:53:44 +0700

    feat: add GetUserGroupsByCronType and UpdateUserGroupsRank interface

--- core/repository/user-group.go
+++ core/repository/user-group.go
@@ -7,4 +7,6 @@ import (
 
 type UserGroupRepository interface {
 	UpsertUserGroup(ctx context.Context, request *entity.UpsertUserGroupRequest) error
+	GetUserGroupsByCronType(ctx context.Context, cronType entity.CronType) ([]*entity.UserGroup, error)
+	UpdateUserGroupsRank(ctx context.Context, userGroups []*entity.UserGroup) error
 }
