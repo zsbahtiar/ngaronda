9fcacb1805fd868dc6152a6313cb3ea1245f0e96
Zam Zam Saeful Bahtiar <zamzam.saeful@efishery.com>
2022-12-16 08:06:58 +0700

    feat: add redis and cron to config

--- config/config.go
+++ config/config.go
@@ -6,10 +6,15 @@ import (
 )
 
 type Config struct {
-	SlackBaseURL string `envconfig:"SLACK_BASE_URL" default:""`
-	SlackAPIKey  string `envconfig:"SLACK_API_KEY" default:""`
-	HTTPPort     string `envconfig:"HTTP_PORT" default:"8080"`
-	Database     Database
+	SlackBaseURL  string `envconfig:"SLACK_BASE_URL" default:""`
+	SlackAPIKey   string `envconfig:"SLACK_API_KEY" default:""`
+	HTTPPort      string `envconfig:"HTTP_PORT" default:"8080"`
+	Database      Database
+	RedisHostName string `envconfig:"REDIS_HOST_NAME" default:"localhost"`
+	RedisUserName string `envconfig:"REDIS_USER_NAME" default:""`
+	RedisPassword string `envconfig:"REDIS_PASSWORD" default:""`
+	RedisPort     int    `envconfig:"REDIS_PORT" default:"6379"`
+	ScheduleCron  ScheduleCron
 }
 
 type Database struct {
@@ -18,6 +23,10 @@ type Database struct {
 	DBPassword string `envconfig:"DB_PASSWORD" default:""`
 	DBPort     string `envconfig:"DB_PORT" default:""`
 	DBName     string `envconfig:"DB_NAME" default:""`
+}
+
+type ScheduleCron struct {
+	UpdateUsersGroup string `envconfig:"UPDATE_USERS_GROUP_CRON" default:"* * * * *"`
 }
 
 func Get() Config {
